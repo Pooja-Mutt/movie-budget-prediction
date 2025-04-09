2a80344acdee37b08c1f198d103d9081650f5576
Pooja Mutt <poojahm.ec@gmail.com>
2025-04-09 17:20:06 -0400

    Fix: correct Flask app and start function

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-# This is a sample Python script.
-import Flask
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 from flask import Flask
-app=Flask(__name__)
 
+app = Flask(__name__)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+@app.route('/')
+def home():
+    return "Hello from your Movie Budget Prediction App!"
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+    app.run(debug=True)
