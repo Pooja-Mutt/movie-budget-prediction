d3cb0d3c31bd4e9b5b9d3a8e163428ca892b1f70
Pooja Mutt <poojahm.ec@gmail.com>
2025-04-09 22:28:47 -0400

    Add /about route

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def home():
     return "Hello from your Movie Budget Prediction App!"
 
+@app.route("/about")
+def about():
+    return "This is a Movie Budget Prediction App built with ❤️ using Flask!"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
